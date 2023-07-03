abcdae1ba6eecb9128b9d18dfad0786c7f3d5abb
tugi <blueqwinn@gmail.com>
2023-07-03 22:42:32 +0300

    add routes and seed

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,15 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "workers#index"
+
+  
+  resources :companies
+  get 'workers' to: 'workers#index'
+  post'workers' to: 'workers#create'
+
+
 end
+
+
+
