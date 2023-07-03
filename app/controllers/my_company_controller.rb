54a404393167e285d65decdd4708c7370452b4c2
tugi <blueqwinn@gmail.com>
2023-07-03 22:21:57 +0300

    add controllers

---
+++ app/controllers/my_company_controller.rb
@@ -0,0 +1,2 @@
+class MyCompanyController < ApplicationController
+end
---
+++ test/controllers/my_company_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class MyCompanyControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
