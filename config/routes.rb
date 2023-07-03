a70069574d92b306b0f3d2617bba884e87e655c7
tugi <blueqwinn@gmail.com>
2023-07-03 22:48:51 +0300

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,5 @@
 Rails.application.routes.draw do
-  get 'my/Company'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  root "workers#index"
-
-  
-  resources :companies
-  get 'workers' to: 'workers#index'
-  post'workers' to: 'workers#create'
-
-
+  root 'home#index'
+  get 'workers', to: 'workerss#index'
+  post 'workers', to: 'workerss#create'
 end
-
-
-
