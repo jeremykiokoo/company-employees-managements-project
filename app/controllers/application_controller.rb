a569ba21d59ed616fb2be45880312cd38687d357
tugi <blueqwinn@gmail.com>
2023-07-04 10:54:56 +0300

    add application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgeryr authentication with: :exception
 
-    
+  before_action :authenticate_user!
+
+  private
+
+  def authenticate_user!
+    # Your authentication logic here
+
+
+   # Redirect to login page if worker is user is not authenticated
+  end
 end
