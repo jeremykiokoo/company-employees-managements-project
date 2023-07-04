715fdf4268f423c4757653c25ed9d9d50d035faa
tugi <blueqwinn@gmail.com>
2023-07-04 10:04:58 +0300

    add def update

--- app/controllers/MyCompanyController.rb
+++ app/controllers/MyCompanyController.rb
@@ -3,6 +3,10 @@ def index
     @company = Company.all
     @message = "Welcome to the homepage!"
 end
+
+def create
+
+end
 def show
     @company = Company.find(params[:id])
     render json: company, serializer: CompanySerializer
@@ -11,12 +15,16 @@ def about
     @company_name = 'My Awesome Company'
     @year_founded = 2019
 
-    @descriptin = "This  page contains what the company is about"
+    @description = "This  page contains what the company is about"
  end
 end
 
 def update
-    
+     @company = Company.find(params[:id])
+
+     if @company.update(company_params)
+        redirect_to @company, notice: 'company  data updated successfully'
+
 end
 
   end
