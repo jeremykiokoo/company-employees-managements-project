fc078dda48064aba807906258b15962ebde78a61
tugi <blueqwinn@gmail.com>
2023-07-04 09:14:51 +0300

    add def about

--- app/controllers/my_company_controller.rb
+++ app/controllers/my_company_controller.rb
@@ -1,17 +1,22 @@
 class MyCompanyController < ApplicationController
 def index
+    @company = Company.all
+    @message = "Welcome to the homepage!"
 end
 def show
     company = Company.find(params[:id])
     render json: company, serializer: CompanySerializer
 
+def about
+    @company_name = 'My Awesome Company'
+    @year_founded = 2019
+
+    @descriptin = "This  page contains what the company is about"
+ end
+end
+
 def update
 end
-
-def about
-end
-
-
 
   end
 end
