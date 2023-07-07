0823d3c418e862ca637a91bd1250a2d31b85887c
jeremykioko <jeremy.kioko@student.moringaschool.com>
2023-07-07 11:25:14 +0300

    website

--- client/src/Page/Dashboard/Header.js
+++ client/src/Page/Dashboard/Header.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
-function Header() {
-    return(
-        <>    
-        <h2>Company Management</h2>
-        </>
+function Header({ setIsAdding }) {
+    return (
+        <header>
+            <h1>Company Management</h1>
+            <div style={{ marginTop: '30px', marginBottom: '18px' }}>
+                <button onClick={() => setIsAdding(true)} className='round-button'>Add Button</button>
+            </div>
+        </header>
     )
-}  
+}
 
-export default Header; 
+export default Header
