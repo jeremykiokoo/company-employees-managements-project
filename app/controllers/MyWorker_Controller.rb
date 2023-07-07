3bff5b86cb87f7c170919c5597093a5ec0e13386
tugi <blueqwinn@gmail.com>
2023-07-07 12:51:59 +0300

    changed workercontroller

--- app/controllers/MyWorker_Controller.rb
+++ app/controllers/MyWorker_Controller.rb
@@ -1,6 +1,6 @@
 class MyJobWorkerController < ApplicationController
     def index
-       @job_worker = Job_worker.all
+       @Worker = Worker.all
        before_action :authenticate_user!
 
     end
